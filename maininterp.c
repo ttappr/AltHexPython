d9b8778b99236407302759afab0d5951f073528b
Todd <tmtappr@gmail.com>
2019-02-02 15:28:21 -0800

    Able to import GLibgit add .

--- maininterp.c
+++ maininterp.c
@@ -42,6 +42,7 @@
  */
 typedef struct {
     PyObject_HEAD
+    PyObject      *tscap;
     PyThreadState *threadstate;
 } MainInterpObj;
 
@@ -53,20 +54,17 @@ MainInterpObj *main_interp = NULL;
 static PyObject *MainInterp_new      (PyTypeObject *, PyObject *, PyObject *);
 static int      MainInterp_init      (MainInterpObj *, PyObject *, PyObject *);
 static void     MainInterp_dealloc   (MainInterpObj *);
+static PyObject *MainInterp_import   (MainInterpObj *, PyObject *);
+static PyObject *MainInterp_exec     (MainInterpObj *, PyObject *);
 static PyObject *MainInterp_getattro (MainInterpObj *, PyObject *);
 
-
-/**
- * Accessors.
- */
-/*
-static PyGetSetDef MainInterp_accessors[] = {
-  { "server_time_utc",   (getter)MainInterp_get_time,
-                         (setter)MainInterp_set_time,
-    "Server time.", NULL },
-    { NULL }
+static PyMethodDef MainInterp_methods[] = {
+        {"loadmodule",   (PyCFunction)MainInterp_import, METH_VARARGS,
+         "Imports a module into the main interpreter environment."},
+        {"exec",         (PyCFunction)MainInterp_exec,   METH_VARARGS,
+         "Executes string in main interpreter environment."},
+        {NULL}
 };
-*/
 
 /**
  * Type declaration/instance.
@@ -81,6 +79,7 @@ static PyTypeObject MainInterpType = {
     .tp_new         = MainInterp_new,
     .tp_init        = (initproc)MainInterp_init,
     .tp_dealloc     = (destructor)MainInterp_dealloc,
+    .tp_methods     = MainInterp_methods,
     //.tp_getset      = MainInterp_accessors,
     .tp_getattro    = (getattrofunc)MainInterp_getattro,
 };
@@ -90,13 +89,19 @@ static PyTypeObject MainInterpType = {
  */
 PyTypeObject *MainInterpTypePtr = &MainInterpType;
 
+/**
+ * Always returns the singleton instance of the main interp object.
+ */
 PyObject *
 MainInterp_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
     if (!main_interp) {
+        // tp_alloc() sets refcount to 1.
         main_interp = (MainInterpObj *)type->tp_alloc(type, 0);
         if (main_interp) {
             main_interp->threadstate = py_g_main_threadstate;
+            main_interp->tscap       = PyCapsule_New(py_g_main_threadstate,
+                                                     "interp", NULL);
         }
         else {
             return NULL;
@@ -105,6 +110,10 @@ MainInterp_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     return (PyObject *)main_interp;
 }
 
+/**
+ * Dummy init function. Does nothing except the singleton constructor is
+ * invoked with no arguments.
+ */
 int
 MainInterp_init(MainInterpObj *self, PyObject *args, PyObject *kwargs)
 {
@@ -114,22 +123,101 @@ MainInterp_init(MainInterpObj *self, PyObject *args, PyObject *kwargs)
     return 0;
 }
 
+/**
+ * Destructor. Since there's a static pointer to the singleton instance with
+ * refcount always greater than 1, this shouldn't get called for the duration
+ * of the HexChat process.
+ */
 void
 MainInterp_dealloc(MainInterpObj *self)
 {
     Py_TYPE(self)->tp_free((PyObject *) self);
 }
 
+PyObject *
+MainInterp_import   (MainInterpObj *self, PyObject *args)
+{
+    PyObject     *pyname;
+    PyObject     *pymod;
+    PyObject     *pyexc_type, *pyexc, *pytraceback;
+    SwitchTSInfo tsinfo;
 
+    if (!PyArg_ParseTuple(args, "U", &pyname)) {
+        return NULL;
+    }
+
+    tsinfo = switch_threadstate(self->threadstate);
+
+    pymod = PyImport_Import(pyname);
+
+    if (!pymod) {
+        PyErr_Fetch(&pyexc_type, &pyexc, &pytraceback);
+        PyErr_NormalizeException(&pyexc_type, &pyexc, &pytraceback);
+    }
+
+    switch_threadstate_back(tsinfo);
+
+    if (!pymod) {
+        PyErr_Restore(pyexc_type, pyexc, pytraceback);
+    }
+
+    return pymod;
+}
+
+PyObject *
+MainInterp_exec(MainInterpObj *self, PyObject *args)
+{
+    PyObject     *pyscript;
+    PyObject     *pyret;
+    PyObject     *pyexc_type, *pyexc, *pytraceback;
+    SwitchTSInfo tsinfo;
+    int          retval;
+
+    if (!PyArg_ParseTuple(args, "U", &pyscript)) {
+        return NULL;
+    }
+
+    tsinfo = switch_threadstate(self->threadstate);
+
+    retval = PyRun_SimpleString(PyUnicode_AsUTF8(pyscript));
+
+    if (retval) {
+        PyErr_Fetch(&pyexc_type, &pyexc, &pytraceback);
+        PyErr_NormalizeException(&pyexc_type, &pyexc, &pytraceback);
+    }
+
+    switch_threadstate_back(tsinfo);
+
+    if (retval) {
+        PyErr_Restore(pyexc_type, pyexc, pytraceback);
+        pyret = NULL;
+    }
+    else {
+        pyret = Py_None;
+        Py_INCREF(pyret);
+    }
+
+    return pyret;
+}
+
+/**
+ * Provides access to the objects within __main__ of the main interp.
+ */
 PyObject *
 MainInterp_getattro(MainInterpObj *self, PyObject *pyname)
 {
-    PyObject *pyretval;
-    PyObject *pydir;
+    PyObject     *pyret;
+    PyObject     *pydict;
+    PyObject     *pymain;
+    PyObject     *pyexc_type;
+    PyObject     *pyexc;
+    PyObject     *pytraceback;
+    int          set_name_error = 0;
+    SwitchTSInfo tsinfo;
 
     // See if the requested attribute is defined elsewhere first.
-    if ((pyretval = PyObject_GenericGetAttr((PyObject *)self, pyname))) {
-        return pyretval;
+    if ((pyret = PyObject_GenericGetAttr((PyObject *)self, pyname))) {
+        return pyret;
     }
     // An AttributeError is expected if the attribute isn't one of the generics.
 
@@ -143,7 +231,51 @@ MainInterp_getattro(MainInterpObj *self, PyObject *pyname)
     // Need to switch to the main interp threadstate and check the root
     // dict for the attribute being requested.
 
-    Py_RETURN_NONE;
+    tsinfo = switch_threadstate(self->threadstate);
+
+    pymain = PyImport_AddModule("__main__");
+    pydict = PyModule_GetDict(pymain);
+
+    pyret  = PyDict_GetItemWithError(pydict, pyname);
+
+    if (PyErr_Occurred()) {
+        // If error, capture exception data, clearing it in the target interp.
+        PyErr_Fetch(&pyexc_type, &pyexc, &pytraceback);
+        PyErr_NormalizeException(&pyexc_type, &pyexc, &pytraceback);
+    }
+    else if (!pyret) {
+        set_name_error = 1;
+    }
+    else {
+        Py_INCREF(pyret);
+    }
+
+    switch_threadstate_back(tsinfo);
+
+    if (!pyret) {
+        if (set_name_error) {
+            PyErr_Format(PyExc_NameError, "name '%S' is not defined", pyname);
+        }
+        else {
+            // If error, restore the exception in the calling interp.
+            PyErr_Restore(pyexc_type, pyexc, pytraceback);
+        }
+    }
+
+    return pyret;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
